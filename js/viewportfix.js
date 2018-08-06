1b6ce0e0be92a9ff911c895d9deb560c60d89d4e
Lutsman_win <333333y@gmail.com>
2018-08-06 08:14:11 +0530

    IE fix remove

--- js/viewportfix.js
+++ js/viewportfix.js
@@ -8,7 +8,7 @@
         var body = document.body;
         var width = document.documentElement.clientWidth;
         var zoom = 0.67;
-        if (width > 1200 && width <= 1920) {
+        if (width > 1200 && width <= 1920 && getIEVersion() === 0) {
             if (width > 1400) {
                 zoom += ((width - 1400) / (520/33)) / 100;
             }
@@ -18,4 +18,21 @@
             body.style.zoom = '';
         }
     } 
+
+    function getIEVersion() {
+        var sAgent = window.navigator.userAgent;
+        var Idx = sAgent.indexOf("MSIE");
+      
+        // If IE, return version number.
+        if (Idx > 0) 
+          return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+      
+        // If IE 11 then look for Updated user agent string.
+        else if (!!navigator.userAgent.match(/Trident\/7\./)) 
+          return 11;
+      
+        else
+          return 0; //It is not IE
+    }
+
 })();
